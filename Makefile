d8ed8a9d81a59c294e97967b2be76c133c613364
Ivan Zhuck <i.a.zhuck@yandex.ru>
2023-08-03 13:08:43 +1000

    Added site and env boilerplate

---
+++ Makefile
@@ -0,0 +1,17 @@
+up:
+	docker compose up -d
+
+up-dev:
+	docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+
+down:
+	docker compose down
+
+bash:
+	docker compose exec app bash
+
+phpcs:
+	./vendor/bin/phpcs
+
+wp:
+	docker compose exec app bash -c "wp $(WP_CLI_ACTION)"
---
+++ config/environments/production.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Configuration overrides for WP_ENV === 'production'
+ */
+
+use Roots\WPConfig\Config;
+
+Config::define('SAVEQUERIES', false);
+Config::define('WP_DEBUG', false);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('WP_DEBUG_LOG', false);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', false);
+Config::define('SCRIPT_DEBUG', false);
+Config::define('DISALLOW_INDEXING', false);
+
+ini_set('display_errors', '0');
+
+// Disable plugin and theme updates and installation from the admin
+Config::define('DISALLOW_FILE_MODS', true);
